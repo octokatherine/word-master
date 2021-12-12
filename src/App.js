8e6e0ba09ad8d777dc3aee7463d46307b22bc6ae
Katherine Peterson <octokatherine@github.com>
2021-12-11 23:32:17 -0800

    add cell styles

--- src/App.js
+++ src/App.js
@@ -38,13 +38,17 @@ const status = {
 
 function App() {
   const [board, setBoard] = useState([
-    ['', '', '', '', ''],
-    ['', '', '', '', ''],
+    ['R', 'A', 'I', 'S', ''],
+    ['A', 'N', 'O', '', ''],
     ['', '', '', '', ''],
     ['', '', '', '', ''],
     ['', '', '', '', ''],
     ['', '', '', '', ''],
   ])
+  const [cellStatuses, setCellStatuses] = useState(() =>
+    Array(6).fill(Array(5).fill(status.unguessed))
+  )
+  const [currentRow, setCurrentRow] = useState(0)
   const [letterStatuses, setLetterStatuses] = useState(() => {
     const letterStatuses = {}
     letters.forEach((letter) => {
@@ -53,15 +57,39 @@ function App() {
     return letterStatuses
   })
 
+  const getCellStyles = (rowNumber, colNumber, letter) => {
+    if (rowNumber === currentRow) {
+      if (letter) return 'border-gray-500'
+      return
+    }
+
+    switch (cellStatuses[rowNumber][colNumber]) {
+      case status.green:
+        return 'bg-green-600 text-white'
+      case status.yellow:
+        return 'bg-yellow-500 text-white'
+      case status.gray:
+        return 'bg-gray-600 text-white'
+      default:
+        return
+    }
+  }
+
   return (
     <div className="flex flex-col justify-between h-screen">
       <h1 className="text-center font-medium text-2xl my-2">Word Master</h1>
       <div className="flex items-center flex-col">
-        <div className="grid grid-rows-6 grid-flow-col gap-1">
-          {board.map((row) =>
-            row.map((cell) => (
-              <span className="inline-block w-16 h-16 sm:w-20 sm:h-20 border-2 border-gray-300">
-                {cell}
+        <div className="grid grid-cols-5 grid-flow-row gap-1">
+          {board.map((row, rowNumber) =>
+            row.map((letter, colNumber) => (
+              <span
+                className={`${getCellStyles(
+                  rowNumber,
+                  colNumber,
+                  letter
+                )} inline-flex items-center justify-center font-bold text-3xl w-16 h-16 sm:w-20 sm:h-20 border-2 border-gray-300`}
+              >
+                {letter}
               </span>
             ))
           )}
@@ -82,11 +110,11 @@ const Keyboard = ({ letterStatuses }) => {
   const getKeyStyle = (letter) => {
     switch (letterStatuses[letter]) {
       case status.green:
-        return 'bg-green-500 text-white'
+        return 'bg-green-600 text-white'
       case status.yellow:
         return 'bg-yellow-600 text-white'
       case status.gray:
-        return 'bg-gray-700 text-white'
+        return 'bg-gray-600 text-white'
       default:
         return 'bg-gray-300'
     }
@@ -95,7 +123,7 @@ const Keyboard = ({ letterStatuses }) => {
   return (
     <div className="w-full flex flex-col items-center mb-2">
       {keyboardLetters.map((row, idx) => (
-        <div className="w-full flex justify-center my-[6px]">
+        <div className="w-full flex justify-center my-[5px]">
           <>
             {idx === 2 && (
               <button className="h-14 w-12 px-1 text-xs sm:w-10 bg-gray-300 mx-[2px] font-bold rounded">
