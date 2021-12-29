77ee6aa8e1752fa6386abaf5c6d3f67530a600e9
Katherine Peterson <octokatherine@github.com>
2021-12-29 13:46:12 -0800

    add instruction modal

--- src/App.js
+++ src/App.js
@@ -327,15 +327,51 @@ function App() {
         contentLabel="Game Info Modal"
       >
         <button
-          className="absolute top-4 right-4 rounded-full nm-flat-background text-primary p-2"
+          className="absolute top-4 right-4 rounded-full nm-flat-background text-primary p-1 w-6 h-6 sm:p-2 sm:h-8 sm:w-8"
           onClick={() => setInfoModalIsOpen(false)}
         >
           <Close />
         </button>
-        <div className="h-full flex flex-col items-center justify-center max-w-[300px] mx-auto pt-9 text-primary">
-          <div className="flex-1 w-full border border-red-500">game instructions</div>
-          <div className="flex justify-center">
-            I'm Katherine <Github /> I'm super cool
+        <div className="h-full flex flex-col items-center justify-center max-w-[390px] mx-auto pt-9 text-primary">
+          <div className="flex-1 w-full border sm:text-base text-sm">
+            <h1 className="text-center sm:text-3xl text-2xl">How to play</h1>
+            <ul className="list-disc pl-5 block sm:text-base text-sm">
+              <li className="mt-6 mb-2">You have 6 guesses to guess the correct word.</li>
+              <li className="mb-2">You can guess any valid word.</li>
+              <li className="mb-2">
+                After each guess, each letter will turn green, yellow, or gray.
+              </li>
+            </ul>
+            <div className="mb-3 mt-8 flex items-center">
+              <span className="nm-inset-n-green text-gray-50 inline-flex items-center justify-center text-3x w-10 h-10 rounded-full">
+                W
+              </span>
+              <span className="mx-2">=</span>
+              <span>Correct letter, correct spot</span>
+            </div>
+            <div className="mb-3">
+              <span className="nm-inset-yellow-500 text-gray-50 inline-flex items-center justify-center text-3x w-10 h-10 rounded-full">
+                W
+              </span>
+              <span className="mx-2">=</span>
+              <span>Correct letter, wrong spot</span>
+            </div>
+            <span className="nm-inset-n-gray text-gray-50 inline-flex items-center justify-center text-3x w-10 h-10 rounded-full">
+              W
+            </span>
+            <span className="mx-2">=</span>
+            <span>Wrong letter</span>
+          </div>
+          <div className="flex justify-center sm:text-base text-sm">
+            <span>This project is open source on</span>
+            <a
+              className="ml-[6px] rounded-full h-5 w-5 sm:h-6 sm:w-6"
+              href="https://github.com/octokatherine/word-master"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Github />
+            </a>
           </div>
         </div>
       </Modal>
