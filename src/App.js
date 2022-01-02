b64d9286e9c2aed69c4e822efbb4c9a306a7bcf6
Wilson <61821556+wilsonych@users.noreply.github.com>
2022-01-02 13:54:02 +0800

    Apply suggestions from code review
    Co-authored-by: Katherine Peterson <49968061+octokatherine@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -333,13 +333,13 @@ function App() {
       />
       <Modal
         isOpen={infoModalIsOpen}
-        onRequestClose={() => handleInfoClose()}
+        onRequestClose={handleInfoClose}
         style={customStyles}
         contentLabel="Game Info Modal"
       >
         <button
           className="absolute top-4 right-4 rounded-full nm-flat-background text-primary p-1 w-6 h-6 sm:p-2 sm:h-8 sm:w-8"
-          onClick={() => handleInfoClose()}
+          onClick={handleInfoClose}
         >
           <Close />
         </button>
