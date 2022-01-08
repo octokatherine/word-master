afa596bead6d58d8ed25b54c1c566e57c797804e
Katherine Peterson <octokatherine@github.com>
2022-01-08 10:31:24 -0800

    fix info modal layout

--- src/App.js
+++ src/App.js
@@ -373,7 +373,7 @@ function App() {
           style={customStyles}
           contentLabel="Game Info Modal"
         >
-          <div className={darkMode ? 'dark' : ''}>
+          <div className={`h-full ${darkMode ? 'dark' : ''}`}>
             <button
               className="absolute top-4 right-4 rounded-full nm-flat-background dark:nm-flat-background-dark text-primary dark:text-primary-dark p-1 w-6 h-6 sm:p-2 sm:h-8 sm:w-8"
               onClick={handleInfoClose}
