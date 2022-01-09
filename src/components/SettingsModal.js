933156d6afc9ddecc3eff98bc18c69161c597f55
Nick Oelsner <noelsner@gmail.com>
2022-01-09 10:38:08 -0800

    add ui for difficulty level select

--- src/components/SettingsModal.js
+++ src/components/SettingsModal.js
@@ -1,6 +1,6 @@
 import { ReactComponent as Close } from '../data/Close.svg'
 import Modal from 'react-modal'
-import { Switch } from '@headlessui/react'
+import { Switch, RadioGroup } from '@headlessui/react'
 
 Modal.setAppElement('#root')
 
@@ -19,7 +19,7 @@ export const SettingsModal = ({ isOpen, handleClose, styles, darkMode, toggleDar
           <h1 className="text-center mb-4 sm:text-3xl text-2xl">Settings</h1>
           <div className="flex-1 w-full border-b border-slate-400 mb-4">
             <button
-              className="absolute top-4 right-4 rounded-full nm-flat-background dark:nm-flat-background-dark text-primary dark:text-primary-dark p-1 w-6 h-6 sm:p-2 sm:h-8 sm:w-8"
+              className="absolute top-4 right-4 rounded-full nm-flat-background dark:nm-flat-background-dark text-primary dark:text-primary-dark p-1 w-6 h-6 sm:p-2 sm:h-8 sm:w-8 hover:nm-inset-background dark:hover:nm-inset-background-dark"
               onClick={handleClose}
             >
               <Close />
@@ -46,6 +46,31 @@ export const SettingsModal = ({ isOpen, handleClose, styles, darkMode, toggleDar
                 Dark Mode
               </Switch.Label>
             </Switch.Group>
+
+            {/*
+            UI switch for selecting difficulty level. Uncomment when difficulty level functionality is implemented
+
+            <RadioGroup value={difficultyLevel} onChange={setDifficultyLevel} className="mt-6">
+              <RadioGroup.Label className="w-full text-center">Difficulty Level</RadioGroup.Label>
+              <div className="grid grid-cols-3 gap-2 rounded-full mt-2 p-1 nm-inset-background dark:nm-inset-background-dark">
+                {Object.keys(difficultyOptions).map((option) => (
+                  <RadioGroup.Option
+                    key={option}
+                    value={option}
+                    className={({ checked }) =>
+                      `text-primary dark:text-primary-dark ${
+                        checked
+                          ? 'bg-white dark:text-primary'
+                          : 'hover:nm-inset-background-sm dark:hover:nm-inset-background-dark-sm'
+                      }
+                        rounded-full py-2 px-3 flex items-center justify-center text-sm font-bold uppercase sm:flex-1 cursor-pointer`
+                    }
+                  >
+                    <RadioGroup.Label as="p">{option}</RadioGroup.Label>
+                  </RadioGroup.Option>
+                ))}
+              </div>
+            </RadioGroup>*/}
           </div>
           <div className="flex flex-col items-center">
             <div className="mb-4">
@@ -63,7 +88,7 @@ export const SettingsModal = ({ isOpen, handleClose, styles, darkMode, toggleDar
               href="https://www.buymeacoffee.com/katherinecodes"
               target="_blank"
               rel="noreferrer"
-              className="nm-flat-background dark:nm-flat-background-dark rounded-xl"
+              className="nm-flat-background dark:nm-flat-background-dark rounded-xl hover:nm-inset-background dark:hover:nm-inset-background-dark"
             >
               <img
                 alt="buy me a coffee"
