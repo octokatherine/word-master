35349b4db39fcd07f038aec833bd09f609c023c5
Steven Loria <sloria1@gmail.com>
2022-01-25 23:15:43 -0500

    Move custom font-family to tailwind config

--- src/App.js
+++ src/App.js
@@ -334,7 +334,7 @@ function App() {
           >
             <Settings />
           </button>
-          <h1 className="flex-1 text-center text-xl xxs:text-2xl sm:text-4xl tracking-wide font-bold font-righteous">
+          <h1 className="flex-1 text-center text-xl xxs:text-2xl sm:text-4xl tracking-wide font-bold font-brand">
             DACTLE
           </h1>
           <button
