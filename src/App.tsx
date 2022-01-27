106bc01174cb00ec5fab10b1c0b1b200e5f23d92
Ian C. Anderson <ian@iancanderson.com>
2022-01-26 20:53:58 -0500

    NERDLE

--- src/App.tsx
+++ src/App.tsx
@@ -434,7 +434,7 @@ function App() {
             <Settings />
           </button>
           <h1 className="flex-1 text-center text-xl xxs:text-2xl sm:text-4xl tracking-wide font-bold font-righteous">
-            WORD MASTER
+            NERDLE
           </h1>
           <button
             type="button"
