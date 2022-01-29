507758d52d2fc12cb40396d309c0ac206e1b7052
Steven Loria <sloria1@gmail.com>
2022-01-29 12:48:42 -0500

    Reduce padding to give more space to keyboard

--- src/App.tsx
+++ src/App.tsx
@@ -341,7 +341,7 @@ function App() {
   return (
     <div className={darkMode ? 'dark' : ''}>
       <div className={`flex flex-col justify-between h-fill bg-background dark:bg-background-dark`}>
-        <header className="flex items-center py-2 px-3 text-primary dark:text-primary-dark">
+        <header className="flex items-center py-1 sm:py-2 px-3 text-primary dark:text-primary-dark">
           <button
             type="button"
             onClick={() => setSettingsModalIsOpen(true)}
@@ -360,7 +360,7 @@ function App() {
             <Info />
           </button>
         </header>
-        <div className="flex items-center flex-col py-3 flex-1 justify-center relative">
+        <div className="flex items-center flex-col pb-3 flex-1 justify-center relative">
           <div className="relative">
             <div className="grid grid-cols-5 grid-flow-row gap-4">
               {board.map((row: string[], rowNumber: number) =>
