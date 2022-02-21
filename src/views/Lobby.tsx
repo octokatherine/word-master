4a10350763db5f42391489780db9dd3e6b0b8ab5
gabriel <gabriel.gonzalvez@verys.com>
2022-02-21 04:49:01 -0800

    implement simple modal ...
    * install react-modal package
    * create basic modal behavior for lobby
    * implement lobby modal menu

--- src/views/Lobby.tsx
+++ src/views/Lobby.tsx
@@ -34,14 +34,31 @@ const modalStyle = {
 
 const Lobby = ({}: Props) => {
     const { user } = useStore();
+
     const [isOpenMatch, setIsOpenMatch] = useState(false);
     const [isSpecificPlayer, setSpecificPlayer] = useState(false);
+    const [openMatchLink, setOpenMatchLink] = useState('');
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleModalButtonClick = (selection: string) => {
+        if (selection === 'open') {
+            setIsOpenMatch(true);
+            setSpecificPlayer(false);
+        } else if (selection === 'specific') {
+            setIsOpenMatch(false);
+            setSpecificPlayer(true);
+        }
+    }
+
+    const handleStartNewMatch = () => {
+        setIsModalOpen(true);
+    }
 
 	return (
         <Fragment>
             <div className="max-w-6xl flex flex-col gap-y-3 h-full md:gap-x-6 md:flex-row mx-auto py-6 px-4 sm:px-6 lg:px-8">
                 <div className="flex gap-y-2 flex-col p-4 rounded-lg border border-gray-200 shadow-md bg-[#3C2A34] h-max md:basis-4/12">
-                    <div className="">
+                    <div>
                         <h3 className="text-2xl font-bold tracking-tight text-[#F1F1F9]">Welcome back,</h3>
                         {/* TODO: If email is super long, it'll stretch the page */}
                         <h2 className="text-3xl font-bold tracking-tight text-[#15B097]">{user?.email}</h2>
@@ -66,7 +83,7 @@ const Lobby = ({}: Props) => {
                         </div>
                     </div>
 
-                    <button className="bg-[#15B097] hover:bg-green-700 text-[#F1F1F9] font-bold py-2 px-4 rounded w-full">
+                    <button className="bg-[#15B097] hover:bg-green-700 text-[#F1F1F9] font-bold py-2 px-4 rounded w-full" onClick={handleStartNewMatch}>
                         Start a New Match
                     </button>
                 </div>
@@ -75,7 +92,7 @@ const Lobby = ({}: Props) => {
                 <div className="flex flex-col items-center justify-center overflow-scroll p-6 basis-[46rem] md:basis-8/12 bg-[#3C2A34] rounded-lg border border-gray-200 shadow-md">
                     <div className="mx-auto max-w-lg">
                         <h2 className="mb-2 text-2xl font-bold tracking-tight text-[#F1F1F9] dark:text-white">You have no currently active matches.</h2>
-                        <button className="bg-[#15B097] hover:bg-green-700 text-[#F1F1F9] font-bold py-2 px-4 rounded w-full">
+                        <button className="bg-[#15B097] hover:bg-green-700 text-[#F1F1F9] font-bold py-2 px-4 rounded w-full" onClick={handleStartNewMatch}>
                             Start a New Match
                         </button>
                     </div>
@@ -83,50 +100,73 @@ const Lobby = ({}: Props) => {
             </div>
 
             {/* @ts-ignore */}
-            <ReactModal isOpen={true} style={modalStyle} className="flex border-2 border-white rounded-[5rem]">
-                <div className="flex justify-center flex-col text-xs mx-auto gap-y-4 p-6 md:text-base md:gap-y-8 md:p-12 md:max-w-sm">
-                    <h2 className="text-xl text-center font-bold tracking-tight text-[#F1F1F9] md:text-2xl">Start a New Match</h2>    
+            <ReactModal isOpen={isModalOpen} onRequestClose={() => setIsModalOpen(false)} style={modalStyle} className={`flex border-2 border-white rounded-[5rem]`}>
+                <Fragment>
+                    <i className="fixed top-6 right-6 text-6xl not-italic cursor-pointer transition-all hover:text-zinc-500" onClick={() => setIsModalOpen(false)}>X</i>
 
-                    <p>blah blah blah basic rules/instructions. </p>
- 
-                    <button className={'bg-[#15B097] hover:bg-green-700 text-[#F1F1F9] font-bold py-2 px-4 rounded w-full'}>Invite Specific Player</button>
-                    <button className={'bg-[#15B097] hover:bg-green-700 text-[#F1F1F9] font-bold py-2 px-4 rounded w-full'}>Create Open Match</button>
+                    <div className="flex justify-center flex-col text-xs mx-auto gap-y-4 p-6 md:text-base md:gap-y-8 md:p-12 md:max-w-sm">
+                        {(!isSpecificPlayer && !isOpenMatch) && 
+                            <Fragment>
+                                <h2 className="text-xl text-center font-bold tracking-tight text-[#F1F1F9] md:text-2xl">Start a New Match</h2>    
 
-                    {isSpecificPlayer && 
-                        <div className="flex justify-center flex-col">
-                            <div className="flex justify-center flex-col">
-                                <span>Your Word</span>
-                                <input type="text" className="text-black"></input>
-                            </div>
+                                <p>blah blah blah basic rules/instructions.</p>
 
-                            <div className="flex justify-center flex-col md:flex-row">
-                                <input type="radio" className="text-black pd-2" /> Enter user email
-                                <input type="text" className="text-black pd-2" placeholder="User's email"></input>
-                            </div>
-                        </div>
-                    }
+                                <button className={'bg-[#15B097] hover:bg-green-700 text-[#F1F1F9] font-bold py-2 px-4 rounded w-full'} onClick={() => { handleModalButtonClick('specific') }}>Invite Specific Player</button>
 
-                    {isOpenMatch && <div>lmao</div>}
-                    
-                    {/* <div className="flex justify-center flex-col">
-                        <span>Your Word</span>
-                        <input type="text" className="text-black"></input>
+                                <button className={'bg-[#15B097] hover:bg-green-700 text-[#F1F1F9] font-bold py-2 px-4 rounded w-full'} onClick={() => { handleModalButtonClick('open') }}>Create Open Match</button>
+                            </Fragment>
+                        }
+                        {isSpecificPlayer && 
+                            <Fragment>
+                                <h2 className="text-xl text-center font-bold tracking-tight text-[#F1F1F9] md:text-2xl">Invite Specific Player</h2>   
+
+                                <p>Get a match link only you and a specific player can use.</p>
+
+                                <div className="flex justify-center flex-col">
+                                    <span>Your Word</span>
+                                    <input type="text" className="text-black"></input>
+                                </div>
+
+                                <div className="flex justify-center flex-col">
+                                    <span>Enter user email</span>
+                                    <input type="text" className="text-black pd-2" placeholder="User's email"></input>
+                                </div> 
+
+                                {openMatchLink ?
+                                    <input type="text" />
+                                    :
+                                    <div className="flex justify-center flex-col gap-y-2">
+                                        <button className="bg-[#15B097] hover:bg-green-700 text-[#F1F1F9] font-bold py-2 px-4 rounded w-full">Generate Link</button>
+                                        <button className="bg-[#FFCE47] hover:bg-yellow-600 text-black font-bold py-2 px-4 rounded w-full" onClick={() => {
+                                            setIsOpenMatch(false);
+                                            setSpecificPlayer(false);
+                                        }}>Go Back</button>
+                                    </div>
+                                }
+                            </Fragment>
+                        }
+
+                        {isOpenMatch && 
+                            <Fragment>  
+                                <h2 className="text-xl text-center font-bold tracking-tight text-[#F1F1F9] md:text-2xl">Create Open Match</h2>   
+
+                                <p>Play with the first person who opens the link!</p>
+
+                                <div className="flex justify-center flex-col">
+                                    <span>Your Word</span>
+                                    <input type="text" className="text-black"></input>
+                                </div>
+                                <div className="flex justify-center flex-col gap-y-2">
+                                    <button className="bg-[#15B097] hover:bg-green-700 text-[#F1F1F9] font-bold py-2 px-4 rounded w-full">Generate Link</button>
+                                    <button className="bg-[#FFCE47] hover:bg-yellow-600 text-black font-bold py-2 px-4 rounded w-full" onClick={() => {
+                                        setIsOpenMatch(false);
+                                        setSpecificPlayer(false);
+                                    }}>Go Back</button>
+                                </div>
+                            </Fragment>
+                        }
                     </div>
-
-                    <div className="flex justify-center flex-col">
-                        <div className="flex justify-center flex-col md:flex-row">
-                            <input type="radio" className="text-black pd-2" /> Challenge a User
-                            <input type="text" className="text-black pd-2" placeholder="User's email"></input>
-                        </div>
-                        <div className="flex justify-center flex-col">
-                            <span>Send match URL</span>
-                            <span>Play with the first registered user who opens the link!</span>
-                            <input type="text" className="text-black"></input>
-                        </div>
-                    </div>
-
-                    <button>Create Match</button> */}
-                </div>
+                </Fragment>
             </ReactModal>
         </Fragment>
 	)
